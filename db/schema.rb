8a833c56691b3ab87f3e37910af7d06cca5723fd
satpai1215 <pai.satyan@gmail.com>
2013-12-17 17:44:00 -0800

    add index to events on user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131022233837) do
+ActiveRecord::Schema.define(:version => 20131218014210) do
 
   create_table "comments", :force => true do |t|
     t.string   "content"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(:version => 20131022233837) do
     t.integer  "owner_id"
     t.boolean  "allow_venue_suggestion", :default => true
   end
+
+  add_index "events", ["user_id"], :name => "index_events_on_user_id"
 
   create_table "guests", :force => true do |t|
     t.integer  "user_id"
